c56417740ea3ae4f4e21f9a3a4495f555d2133d6
Jandrocks <32751139+Jandrocks@users.noreply.github.com>
2023-06-13 22:42:03 -0400

    cambio titulo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hola Soy</h5>
         <h1>Alejandro Meza</h1>
-        <h5 className="text-light">Front End Developer</h5>
+        <h5 className="text-light">Developer</h5>
         <CTA />
         <HeaderSocial />
         
